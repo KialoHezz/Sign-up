ac57a32c3e53fb56df8463d25abc060d42b4efc6
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-05-08 23:27:07 +0300

    create user model

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class User:
+    '''
+    User class to define user object
+    '''
+
+    def __init__(self,name,email,password):
+        self.name = name
+        self.email = email
+        self.password = password
